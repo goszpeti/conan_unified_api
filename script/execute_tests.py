4a425d9ecad5140cb649fa16a0a1423ac687d7ea
goszpeti <peter.gosztolya@gmail.com>
2024-08-06 00:37:56 +0200

    Try out new tets matrix

---
+++ script/execute_tests.py
@@ -0,0 +1,27 @@
+
+
+import argparse
+import os
+
+conan_major = 1
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--conan_major_version", type=str)
+
+args = parser.parse_args()
+conan_major = args.conan_major_version
+
+test_versions = {
+    "1": ["1.48.0", "1.59.0", "<2"],
+    "2": ["2.0.14", "~2.1.0", "~2.2.0", "~2.3.0", "~2.4.0", "~2.5.0"]
+}
+
+# compatbility
+os.system("pip install setuptools wheel")
+for conan_version in test_versions[conan_major]:
+
+    os.system(f"pip install conan=={conan_version} --use-pep517 --no-build-isolation")
+    if conan_major=="2":
+        os.system(f"pip install conan_server=={conan_version} --use-pep517")
+    os.system(f"pytest -v test --junit-xml=./results/result-unit-{conan_version}.xml --cov=conan_unified_api --cov-branch --cov-append --capture=no")
+    os.environ["SKIP_CREATE_CONAN_TEST_DATA"]="True" #enable after first run
