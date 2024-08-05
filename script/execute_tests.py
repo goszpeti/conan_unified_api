f65d5086d24fd938976b6e82433f9e7391257db7
goszpeti <peter.gosztolya@gmail.com>
2024-08-06 00:52:00 +0200

    Overhaul test calling setup

--- script/execute_tests.py
+++ script/execute_tests.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import subprocess
 
 conan_major = 1
 
@@ -12,16 +13,18 @@ args = parser.parse_args()
 conan_major = args.conan_major_version
 
 test_versions = {
-    "1": ["1.48.0", "1.59.0", "<2"],
-    "2": ["2.0.14", "~2.1.0", "~2.2.0", "~2.3.0", "~2.4.0", "~2.5.0"]
+    "1": ["==1.48.0", "==1.59.0", "<2"],
+    "2": ["==2.0.14", "~=2.1.0", "~=2.2.0", "~=2.3.0", "~=2.4.0", "~=2.5.0"]
 }
 
 # compatbility
 os.system("pip install setuptools wheel")
 for conan_version in test_versions[conan_major]:
 
-    os.system(f"pip install conan=={conan_version} --use-pep517 --no-build-isolation")
+    os.system(f"pip install conan{conan_version} --use-pep517 --no-build-isolation")
     if conan_major=="2":
-        os.system(f"pip install conan_server=={conan_version} --use-pep517")
-    os.system(f"pytest -v test --junit-xml=./results/result-unit-{conan_version}.xml --cov=conan_unified_api --cov-branch --cov-append --capture=no")
-    os.environ["SKIP_CREATE_CONAN_TEST_DATA"]="True" #enable after first run
+        os.system(f"pip install conan_server{conan_version} --use-pep517")
+    subprocess.run(["pytest", "-v", "test", f"--junit-xml=./results/result-unit-{conan_version}.xml",
+                    "--cov=conan_unified_api", "--cov-branch", "--cov-append", "--capture=no"], 
+                    check=True)
+    os.environ["SKIP_CREATE_CONAN_TEST_DATA"]="True" # enable after first run
